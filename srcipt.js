35c88c4b3c0bc3b950329c5765d88c4be80e1e99
Ansh Raiyani <anshraiyani13@gmail.com>
2023-04-01 00:11:47 +0530

    completed js

--- srcipt.js
+++ srcipt.js
@@ -7,6 +7,15 @@ function Book(title,author,pages,read){
     this.read=read
 }
 
+Book.prototype.toggleRead = function(){
+    this.read=!this.read
+}
+
+function toggleRead(index){
+    myLibrary[index].toggleRead()
+    renderbooks()
+}
+
 function addBookToLibrary(){
     let title=document.querySelector("#title").value
     let author=document.querySelector("#author").value
@@ -22,25 +31,21 @@ function renderbooks(){
     library.innerHTML=""
     for(let i=0;i<myLibrary.length;i++){
         let book = myLibrary[i]
-        let bookElement = document.createElement("div")
-        if(book.read){
-            bookElement.innerHTML=`<div class="book-card">
-                <h2>${book.title}</h2>
-                <h3>${book.author}</h3>
-                <h3>${book.pages}</h3>
-                <h3>read</h3>
-            </div>`
-        }
-        else{
-            bookElement.innerHTML=`<div class="book-card">
-            <h2>${book.title}</h2>
+        let bookElement = document.createElement('div')
+        bookElement.className="book-card"
+         bookElement.innerHTML=`<h2>${book.title}</h2>
             <h3>${book.author}</h3>
             <h3>${book.pages}</h3>
-            <h3>not read yet</h3>
-        </div>`
-        }
+            <h3>${book.read ? "Read" : "Not Read Yet"}</h3>
+            <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
+            <button class="read-btn" onclick="toggleRead(${i})">Toggle Read</button>`
         library.appendChild(bookElement)
     }
+}
+
+function removeBook(index){
+    myLibrary.splice(index,1)
+    renderbooks()
 }
 
 document.addEventListener("DOMContentLoaded", function () {
